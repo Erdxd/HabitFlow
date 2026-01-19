f92c6b82a92b4066ddede8237af52da4efa58bd6
unknown <rovarvgag@gmail.com>
2026-01-19 14:35:52 +0700

    .

--- main.go
+++ main.go
@@ -309,7 +309,7 @@ func ScheduleForReset(db *sql.DB) {
 	}
 
 	s := gocron.NewScheduler(anadyrLocation)
-	_, err = s.Every(1).Day().At("19:32").Do(func() {
+	_, err = s.Every(1).Day().At("19:37").Do(func() {
 		database.ResetAllStatus(db)
 
 	})
